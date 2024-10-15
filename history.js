49a6e16c660d97205bfd0d4a9e5db3ef1803afad
stevenmarsack <stevenmarsack2026@gmail.com>
2024-10-15 16:48:41 -0400

    construct matrix

--- history.js
+++ history.js
@@ -1,22 +1,43 @@
 
-//initialize 5 last dreams logged//
-pullDreamIndex();
 let currentHigh=dreamIndex;
 let currentLow=dreamIndex-5;
 let initialDreamOutputs;
+let matrix=[];
+
+pullDreamIndex();
+constructMatrix();
+function constructMatrix(){
+    let matrixTitle=[];
+    let matrixType=[];
+    let matrixDate=[];
+    let matrixFavorite=[];
+    let matrixDescription=[];
+    for (let i=0;i<dreamIndex+1;i++) {
+        matrixTitle.push(localStorage.getItem("title"+i));
+        matrixType.push(localStorage.getItem("type"+i));
+        matrixDate.push(localStorage.getItem("date"+i));
+        matrixFavorite.push(localStorage.getItem("favorite"+i));
+        matrixDescription.push(localStorage.getItem("description"+i));
+    }
+matrix.push(matrixTitle);
+matrix.push(matrixType);
+matrix.push(matrixDate);
+matrix.push(matrixFavorite);
+matrix.push(matrixDescription);
+}
+
 if(dreamIndex>=4){
     initialDreamOutputs=5;
 }else{
     initialDreamOutputs=dreamIndex+1;
 }
 for (let i=0;i<initialDreamOutputs;i++) {
-    document.getElementById("titleOutput"+i).setAttribute("label",localStorage.getItem("title"+(dreamIndex-i)));
-    document.getElementById("typeOutput"+i).setAttribute("label",localStorage.getItem("type"+(dreamIndex-i)));
-    document.getElementById("dateOutput"+i).setAttribute("label",localStorage.getItem("date"+(dreamIndex-i)));
-    document.getElementById("favoriteOutput"+i).setAttribute("label",localStorage.getItem("favorite"+(dreamIndex-i)));
-    document.getElementById("descriptionOutput"+i).setAttribute("label",localStorage.getItem("description"+(dreamIndex-i)));
+    document.getElementById("titleOutput"+i).setAttribute("value",localStorage.getItem("title"+(dreamIndex-i)));
+    //document.getElementById("typeOutput"+i).setAttribute("value",localStorage.getItem("type"+(dreamIndex-i)));
+    document.getElementById("dateOutput"+i).setAttribute("value",localStorage.getItem("date"+(dreamIndex-i)));
+    //document.getElementById("favoriteOutput"+i).setAttribute("value",localStorage.getItem("favorite"+(dreamIndex-i)));
+    document.getElementById("descriptionOutput"+i).setAttribute("value",localStorage.getItem("description"+(dreamIndex-i)));
 }
-//initialize 5 last dreams logged//
 
 document.getElementById("increaseIndexBtn").addEventListener("click", function(){
     if(dreamIndex>=5){
@@ -27,11 +48,11 @@ document.getElementById("increaseIndexBtn").addEventListener("click", function()
             increaseAmmount=dreamIndex-currentHigh
         }
         for (let i=0;i<5;i++) {
-            document.getElementById("titleOutput"+i).setAttribute("label",localStorage.getItem("title"+(currentHigh+increaseAmmount-i)));
-            document.getElementById("typeOutput"+i).setAttribute("label",localStorage.getItem("type"+(currentHigh+increaseAmmount-i)));
-            document.getElementById("dateOutput"+i).setAttribute("label",localStorage.getItem("date"+(currentHigh+increaseAmmount-i)));
-            document.getElementById("favoriteOutput"+i).setAttribute("label",localStorage.getItem("favorite"+(currentHigh+increaseAmmount-i)));
-            document.getElementById("descriptionOutput"+i).setAttribute("label",localStorage.getItem("description"+(currentHigh+increaseAmmount-i)));
+            document.getElementById("titleOutput"+i).setAttribute("value",localStorage.getItem("title"+(currentHigh+increaseAmmount-i)));
+            //document.getElementById("typeOutput"+i).setAttribute("value",localStorage.getItem("type"+(currentHigh+increaseAmmount-i)));
+            document.getElementById("dateOutput"+i).setAttribute("value",localStorage.getItem("date"+(currentHigh+increaseAmmount-i)));
+           // document.getElementById("favoriteOutput"+i).setAttribute("value",localStorage.getItem("favorite"+(currentHigh+increaseAmmount-i)));
+            document.getElementById("descriptionOutput"+i).setAttribute("value",localStorage.getItem("description"+(currentHigh+increaseAmmount-i)));
         }
         currentHigh=currentHigh+increaseAmmount
         currentLow=currentLow+increaseAmmount
@@ -47,11 +68,11 @@ document.getElementById("decreaseIndexBtn").addEventListener("click", function()
             decreaseAmmount=currentLow
         }
         for (let i=4;i>-1;i--) {
-            document.getElementById("titleOutput"+i).setAttribute("label",localStorage.getItem("title"+(currentLow-decreaseAmmount-i+4)));
-            document.getElementById("typeOutput"+i).setAttribute("label",localStorage.getItem("type"+(currentLow-decreaseAmmount-i+4)));
-            document.getElementById("dateOutput"+i).setAttribute("label",localStorage.getItem("date"+(currentLow-decreaseAmmount-i+4)));
-            document.getElementById("favoriteOutput"+i).setAttribute("label",localStorage.getItem("favorite"+(currentLow-decreaseAmmount-i+4)));
-            document.getElementById("descriptionOutput"+i).setAttribute("label",localStorage.getItem("description"+(currentLow-decreaseAmmount-i+4)));
+            document.getElementById("titleOutput"+i).setAttribute("value",localStorage.getItem("title"+(currentLow-decreaseAmmount-i+4)));
+            //document.getElementById("typeOutput"+i).setAttribute("value",localStorage.getItem("type"+(currentLow-decreaseAmmount-i+4)));
+            document.getElementById("dateOutput"+i).setAttribute("value",localStorage.getItem("date"+(currentLow-decreaseAmmount-i+4)));
+            //document.getElementById("favoriteOutput"+i).setAttribute("value",localStorage.getItem("favorite"+(currentLow-decreaseAmmount-i+4)));
+            document.getElementById("descriptionOutput"+i).setAttribute("value",localStorage.getItem("description"+(currentLow-decreaseAmmount-i+4)));
         }
         currentHigh=currentHigh-decreaseAmmount
         currentLow=currentLow-decreaseAmmount
